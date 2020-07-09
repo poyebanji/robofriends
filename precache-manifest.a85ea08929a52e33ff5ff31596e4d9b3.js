e2b91f10ae8297386c7ee5e0570874730291457e
Peter Oyebanji <peter@prob.ca>
2020-07-09 12:46:14 -0600

    Updates

---
+++ precache-manifest.a85ea08929a52e33ff5ff31596e4d9b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f22abbb534c6a017c00d0850c102a60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f1cb15525fc603f5d755",
+    "url": "/static/css/main.680f7b4a.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1cb15525fc603f5d755",
+    "url": "/static/js/main.178e296b.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  }
+]);
